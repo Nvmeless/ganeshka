71e22f2dd62635e03b60f3fd63d4f0027fd7d6ec
Oltean Petru <poltean33@gmail.com>
2022-11-24 16:57:23 +0100

    :)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Card } from './components/atoms/Card/Card';
+import { Grid } from './components/atoms/Grid/Grid';
 
 function App() {
   return (
     <div className="App">
       <Card backgroundColor={"red"} height="10px" width={"100%"}>Meswsage</Card>
+      {/* import grid */}
+      <Grid columns={3} rows={3} gap={10} />
     </div>
   );
 }
