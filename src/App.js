a05e102fabdf22b01e48899b5cf89b91ecb7b519
Gregory <gergory.mostacci@ynov.com>
2022-11-29 21:56:13 +0100

    nolog: Start from the same base magueule

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import {getTheme} from './themes/default.js'
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={getTheme()}>
+        {/* Mets ton element que tu dev ICI */}
+      </ThemeProvider>
     </div>
   );
 }
