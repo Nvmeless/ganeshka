6e523ed28ee9a8541790f2ed19be11be75bdf291
Gregory <gergory.mostacci@ynov.com>
2022-11-29 21:41:46 +0100

    nolog: Remove useless element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Card } from './components/atoms/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      <Card backgroundColor={"red"} height="10px" width={"100%"}>Meswsage</Card>
     </div>
   );
 }
