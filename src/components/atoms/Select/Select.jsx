d11ba5820dba9d86c089a7923705b84aa4e97e4d
laura-nesta <laura.nesta@outlook.com>
2022-12-04 01:33:03 +0100

    init Select de base

--- src/components/atoms/Select/Select.jsx
+++ src/components/atoms/Select/Select.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import {Card} from "../Card/Card"
+import { Button } from "@mui/material";
 //jinn#4465
 
-export const Select = ({...props}) => {
+export const Select = ({backgroundColor,...props}) => {
+    return (
+        <Card width="160px">
+            TextSelecteur
+            <Button variant="text">V</Button>
+        </Card>
+    )
     
 }
 
 Select.defaultProps = {
+     /**
+     * Which color is used for background
+     */
+      backgroundColor: PropTypes.string,
+}
+
+Select.defaultProps = {
+    backgroundColor: null
 }
