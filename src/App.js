23b8e549beda884c498068e6936f5f8ea39129c1
jinn <alexandre.Quilan.delaistre@gmail.com>
2022-12-07 03:40:04 +0100

    *D3 js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import { Card } from './components/atoms/Card/Card';
 import { ThemeProvider } from 'styled-components';
 import {getTheme} from './themes/default.js'
+import D3Scene from './components/organisms/D3Scene/D3Scene.jsx';
 function App() {
   return (
     <ThemeProvider theme={getTheme()}>
     <div className="App">
       Ganeshka toolkit
     </div>
+    <D3Scene></D3Scene>
     </ThemeProvider>
   );
 }
