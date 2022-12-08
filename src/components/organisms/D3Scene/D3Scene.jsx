02e4ff7ce48cce0fba94c0517fa7ea0b9ebe5a31
Nvmeless <59935640+Nvmeless@users.noreply.github.com>
2022-12-08 05:59:32 +0100

    New usage

--- src/components/organisms/D3Scene/D3Scene.jsx
+++ src/components/organisms/D3Scene/D3Scene.jsx
@@ -1,19 +1,39 @@
 import * as d3 from 'd3';
 import { useRef, useEffect, useState  } from 'react';
+import { registerRoute } from 'workbox-routing';
 import useInterval from '../../../hooks/useInterval.tsx'
-const generateDataset = () => (
-  Array(10).fill(0).map(() => ([
-    Math.random() * 400 + 10,
-    Math.random() * 400 + 10,
-  ]))
-)
+
+const move = ({vector, x,y}) => {
+  return {
+
+    y: (() => (y + (Math.random() * (Math.random() < .5) ? .15 : -.15) ))(),
+    x: (() => (x + (Math.random() * (Math.random() < .5) ? .15 : -.15) ))(),
+    vector: vector
+  }
+}
+
+
+const generateDataset = (set, defaultMovement = (x) => x) => {
+  if(set){
+    return (set.map((circle) => {
+      const {vector} = circle
+      return vector(circle); 
+    }))
+  }
+  return (
+  Array(10).fill(0).map(() => ({
+    x:Math.random() * 400 + 10,
+    y:Math.random() * 400 + 10,
+    vector: defaultMovement
+  }))
+)}
 
 
 const D3Scene = ({height = 400 , width = 400, ...props}) => {
 
   const ref = useRef()
   const [dataset, setDataset] = useState(
-    generateDataset()
+    generateDataset(null, move)
   )
 
 
@@ -23,16 +43,16 @@ const D3Scene = ({height = 400 , width = 400, ...props}) => {
     svgElement.selectAll("circle")
       .data(dataset)
       .join("circle")
-        .attr("cx", d => d[0])
-        .attr("cy", d => d[1])
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
         .attr("r",  10)
+        .attr('fill', "red")
   }, [dataset])
 
   useInterval(() => {
-    const newDataset = generateDataset()
-    console.log("Hey")
+    const newDataset = generateDataset(dataset, move)
     setDataset(newDataset)
-  }, 1000)
+  }, 100)
 
   return (
     <svg
