3ef73cf3f13fbede793a8856a1f08b94ba4ec20f
Nvmeless <59935640+Nvmeless@users.noreply.github.com>
2022-12-08 07:37:21 +0100

    PWA - Installable

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import { Card } from './components/atoms/Card/Card';
 import { ThemeProvider } from 'styled-components';
 import {getTheme} from './themes/default.js'
 function App() {
+  fetch('https://api.github.com/users/Nvmeless/repos').then(
+  resp => resp.json() // this returns a promise
+).then(repos => {
+  console.log('REPOS FETCHED')
+  for (const repo of repos) {
+    // console.log(repo.name);
+  }
+}).catch(ex => {
+  console.error(ex);
+})
   return (
     <ThemeProvider theme={getTheme()}>
     <div className="App">
