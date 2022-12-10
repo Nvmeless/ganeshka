aebbfbeff41ed1380670010211f85f5bc80cd783
Kévin <kevin.blaise24@gmail.com>
2022-12-10 16:11:54 +0100

    remove useless style

--- src/App.js
+++ src/App.js
@@ -14,30 +14,30 @@ function App() {
       {/* <Grid container style={{ width: '100vw', height: '100vh' }}>
           <Grid item>
             <Grid container direction="row" wrap={"nowrap"}>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'red' }}>1</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'lightgray' }}>2</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'yellow' }}>3</div>
+              <div style={{ backgroundColor: 'red' }}>1</div>
+              <div style={{ backgroundColor: 'lightgray' }}>2</div>
+              <div style={{ backgroundColor: 'yellow' }}>3</div>
             </Grid>
           </Grid>
           <Grid item>
             <Grid container direction="row-reverse" columnSpacing={2} wrap={"nowrap"}>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'steelblue' }}>1</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'crimson' }}>2</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'tan' }}>3</div>
+              <div style={{ backgroundColor: 'steelblue' }}>1</div>
+              <div style={{ backgroundColor: 'crimson' }}>2</div>
+              <div style={{ backgroundColor: 'tan' }}>3</div>
             </Grid>
           </Grid>
           <Grid item>
             <Grid container direction="column" rowSpacing={2} wrap={"nowrap"}>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'seagreen' }}>1</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'salmon' }}>2</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'powderblue' }}>3</div>
+              <div style={{ backgroundColor: 'seagreen' }}>1</div>
+              <div style={{ backgroundColor: 'salmon' }}>2</div>
+              <div style={{ backgroundColor: 'powderblue' }}>3</div>
             </Grid>
           </Grid>
           <Grid item>
             <Grid container direction="column-reverse" wrap={"nowrap"}>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'orange' }}>1</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'dimgray' }}>2</div>
-              <div style={{ width: '100%', height: '100%', backgroundColor: 'navy' }}>3</div>
+              <div style={{ backgroundColor: 'orange' }}>1</div>
+              <div style={{ backgroundColor: 'dimgray' }}>2</div>
+              <div style={{ backgroundColor: 'navy' }}>3</div>
             </Grid>
           </Grid>
         </Grid> */}
@@ -62,18 +62,18 @@ function App() {
       {/* <Grid container direction={"column"} rowSpacing={2}>
         <Grid xs={12} md={12} spacing={1} wrap={"nowrap"}>
           <Grid item xs={6} md={8}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'pink' }}>6 / 8</div>
+            <div style={{ backgroundColor: 'pink' }}>6 / 8</div>
           </Grid>
           <Grid item xs={6} md={4}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'cyan' }}>6 / 4</div>
+            <div style={{ backgroundColor: 'cyan' }}>6 / 4</div>
           </Grid>
         </Grid>
         <Grid xs={12} md={12} spacing={1} wrap={"nowrap"}>
           <Grid item xs={6} md={4}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'lightgreen' }}>6 / 4</div>
+            <div style={{ backgroundColor: 'lightgreen' }}>6 / 4</div>
           </Grid>
           <Grid item xs={6} md={8}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'darkkhaki' }}>6 / 8</div>
+            <div style={{ backgroundColor: 'darkkhaki' }}>6 / 8</div>
           </Grid>
         </Grid>
       </Grid> */}
