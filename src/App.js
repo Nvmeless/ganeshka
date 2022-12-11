1a461cd137d3e52cb9e50b1fa1af6a406baff659
julien-guilet <guiletjulien@gmail.com>
2022-12-11 15:27:06 +0100

    reset app.ps

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ import {CiAlarmOn} from "react-icons/ci"
 function App() {
   return (
     <div className="App">
-      {/*<Card backgroundColor={"red"} height="10px" width={"100%"}>Meswsage</Card>*/}
-      <div><Avatar src="./logo512.png"></Avatar>
-      <Avatar size={100} borderRadius={10} textColor={'white'} bgColor={"red"}>Julien Guilet</Avatar>
-      <Avatar size={20}>bonour</Avatar>
-      <Avatar size={25} >Alex Thibeaud</Avatar>
-      <Avatar size={30} borderRadius={10}></Avatar>
-      <Avatar size={30} bgColor={"green"} borderRadius={10}>Max Wacquier</Avatar></div>
+      <Card backgroundColor={"red"} height="10px" width={"100%"}>Meswsage</Card>
     </div>
   );
 }
