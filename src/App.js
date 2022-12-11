e5f72f55a9279dd71724202decdf0f871d9cb21f
BrunoWOLFF-Ynov <bruno.wolffdumontel@ynov.com>
2022-12-11 19:14:50 +0100

    Switch button
    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Card } from './components/atoms/Card/Card';
+import { Switch } from './components/atoms/switch/Switch';
+
+global.useState = React.useState
 
 function App() {
+  var [toggle, setToggle] = React.useState(false);
+
   return (
     <div className="App">
-      <Card backgroundColor={"red"} height="10px" width={"100%"}>Meswsage</Card>
+      <Switch size='small' label='message' color="#006666" placement="top" checked={toggle} onChange={setToggle}></Switch>
+      <Switch label='message' placement="top" checked={toggle} onChange={setToggle}></Switch>
+      <Switch size='small' iconUnchecked='🌜' iconChecked='🌞' color="#660066" placement="top"></Switch>
+      <Switch labelBefore='off' labelAfter='on' color="#666600" placement="top" defaultChecked></Switch>
     </div>
   );
 }
