165565a927e1ca2503b9cccd9f0ef9977c458409
Nvmeless <59935640+Nvmeless@users.noreply.github.com>
2022-12-12 11:36:55 +0100

    delete AppComponents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Card } from './components/atoms/Card/Card';
-import { Switch } from './components/atoms/switch/Switch';
 
 global.useState = React.useState
 
 function App() {
   return (
     <div className="App">
+      Ganeshka toolkit 
     </div>
   );
 }
