a6588507662726526dc3a2d601006bb4c92b6480
CM <corentin.mailler19@gmail.com>
2022-12-12 10:51:21 +0100

    ajout de commerce.js au projet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Card } from './components/atoms/Card/Card';
 import { ThemeProvider } from 'styled-components';
 import {getTheme} from './themes/default.js'
+import Commerce from '@chec/commerce.js';
+
 function App() {
+  // creation de notre instance commerce
+  const commerce = new Commerce('pk_test_49104637cd5730bad2a0d52cc7f38c2ed5927e9f0368e', true);
+
   return (
     <ThemeProvider theme={getTheme()}>
     <div className="App">
-      Ganeshka toolkit
+        {/* Passer l'instance commerce en props si on veut l'utiliser dans un composant */}
     </div>
     </ThemeProvider>
   );
