72132a4521046e27856c2783458c43531c5adee3
Nvmeless <59935640+Nvmeless@users.noreply.github.com>
2022-12-12 11:32:27 +0100

    delete AppComponents

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import { Switch } from './components/atoms/switch/Switch';
 global.useState = React.useState
 
 function App() {
-  var [toggle, setToggle] = React.useState(false);
-
   return (
     <div className="App">
     </div>
