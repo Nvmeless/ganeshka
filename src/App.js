9b97a8100e36dbbedefdb01b09c5c6ee720b4d7f
PikkMoune71 <antoinepicard385@gmail.com>
2022-12-12 13:45:41 +0100

    creating login and registration pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import { ThemeProvider } from 'styled-components';
 import {getTheme} from './themes/default.js'
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
-import { Login } from './pages/Login/Login';
 import { Provider } from 'react-redux';
 import {store} from './store/store'
+import  Login  from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         </ThemeProvider>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,100 @@
-import React from 'react'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
-export const Login = () => {
+function Copyright(props) {
   return (
-    <div>Login</div>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+        Squirrel
+      {' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+            sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+            </Avatar>
+            <Typography component="h1" variant="h5">
+            Connexion
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Mot de passe"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+            />
+            <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Se souvenir de moi"
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+            >
+                Connexion
+            </Button>
+            <Grid item xs>
+                <Link href="#" variant="body2">
+                    Mot de passe oublié ?
+                </Link>
+            </Grid>
+            <Grid item >
+                <Link href="/register" variant="body2">
+                    {"Vous n'avez pas de compte? Inscrivez-vous"}
+                </Link>
+            </Grid>
+            </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   )
 }
