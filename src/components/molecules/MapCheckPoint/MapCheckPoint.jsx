78fc42e28a93c8ba4eb38ff0704ae4667e8d921d
Julien <julien.guilet@etu.univ-grenoble-alpes.fr>
2022-12-12 17:21:13 +0100

    Ajout MapCheckPoint + commencement Product Card

---
+++ src/components/molecules/MapCheckPoint/MapCheckPoint.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import { StyledMapCheckpointDiv, StyledMapCheckpointIcon } from './style.js'
+
+export const MapCheckPoint = ({...props}) => {
+
+    
+
+    const styleIcon = {
+        width: props?.size ? props.size+"px" : "50px",
+        height: props?.size ? props.size+"px" : "50px",
+        margin:0,
+        padding:0,
+    }
+
+    const styleP = {
+        margin:0,
+        padding:0,
+    }
+
+    return (
+        <>
+            <StyledMapCheckpointDiv >
+                <div>
+                    <FmdGoodIcon style={styleIcon}></FmdGoodIcon>
+                    {props?.text ? <p style={styleP}> {props.text} </p> : ""}
+                </div>
+            </StyledMapCheckpointDiv>
+        </>
+    )
+
+}
---
+++ src/components/molecules/ProductCard/ProductCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Card } from "../../atoms/Card/Card";
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import {StyledProductCard} from './style.js'
+
+export const ProductCard = ({...props}) => {
+
+
+
+    return (
+         <></>
+        // <Card >
+        //     <StyledProductCard>Nom du produit</StyledProductCard>
+        //     <img src="../../logo192.png"></img>
+        //     <p>Description du produit</p>
+        //     <FmdGoodIcon ></FmdGoodIcon>
+        //     <p>150 m</p>
+
+
+        // </Card>
+    )
+    
+}
