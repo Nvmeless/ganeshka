e0b8115495e714bcd976e8c989998d43b079f5b7
PikkMoune71 <antoinepicard385@gmail.com>
2022-12-12 10:48:25 +0100

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Card } from './components/atoms/Card/Card';
 import { ThemeProvider } from 'styled-components';
 import {getTheme} from './themes/default.js'
-import SpeedDialIcon from "./components/atoms/SpeedDialIcon/SpeedDialIcon";
-import SpeedDialAction from "./components/atoms/SpeedDialAction/SpeedDialAction";
-import SpeedDial from "./components/molecules/SpeedDial/SpeedDial"
-import { FaBeer,FaAccessibleIcon,Fa500Px,FaAcquisitionsIncorporated } from 'react-icons/fa';
-
-
-const actions = [
-  { icon: <FaBeer />, name: 'Copy' },
-  { icon: <FaAccessibleIcon />, name: 'Save' },
-  { icon: <Fa500Px />, name: 'Print' },
-  { icon: <FaAcquisitionsIncorporated />, name: 'Share' },
-];
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
+import { Login } from './pages/Login/Login';
 
 function App() {
 
   return (
     <ThemeProvider theme={getTheme()}>
     <div className="App">
-      Ganeshka toolkit
-      <SpeedDial
-          ariaLabel="SpeedDial tooltip example"
-          icon={<SpeedDialIcon />}
-      >
-        {actions.map((action) => (
-            <SpeedDialAction
-                key={action.name}
-                icon={action.icon}
-                tooltipTitle={action.name}
-                tooltipPlacement={"left"}
-            />
-        ))}
-      </SpeedDial>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
     </ThemeProvider>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>Ceci est la Home de Squirrel</div>
+  )
+}
