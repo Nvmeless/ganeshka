440e4d48efe8da2e37124589d2c583d35d27f760
paul martischang <paul-m08@live.fr>
2022-12-12 10:01:46 +0100

    Add paper component (including container)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Card } from './components/atoms/Card/Card';
+import { Paper } from './components/atoms/Paper/Paper';
 import { ThemeProvider } from 'styled-components';
 import {getTheme} from './themes/default.js'
 function App() {
