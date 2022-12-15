530f878e7f1fff585b8a513359cad970ad36218f
PikkMoune71 <antoinepicard385@gmail.com>
2022-12-15 10:55:01 +0100

    create authlogin with redux

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Provider } from 'react-redux';
-import {store} from './store/store'
+import store from './store/store'
 import  Login  from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import { Navbar } from './components/atoms/Navbar/Navbar';
