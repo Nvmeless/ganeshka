4d477e0a6203208703e89239b31a9a67a3c8552e
MatthiasChometon <matthiaschometon787@gmail.com>
2022-12-20 22:34:45 +0100

    feat: add connectedUser

--- src/shared/stores/store.js
+++ src/shared/stores/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notifications from '../notifications/stores/notifications'
+import connectedUser from '../../authentication/login/stores/connectedUser'
 
 export const store = configureStore({
   reducer: {
-    notifications
+    notifications,
+    connectedUser
   },
 })
