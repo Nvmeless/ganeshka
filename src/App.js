42f5db4d4443c953d6afcff4390ec69933aa3309
Orthoceras <florian.berthier@hotmail.com>
2023-01-02 11:45:07 +0100

    fix: space appbar

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <BrowserRouter>
           <Appbar role="professor" />
           <Routing />
+          <div className="appbar_space"></div>
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
