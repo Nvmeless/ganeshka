25f568aaf6e76291ccaac742f5840b2dc0d87a7b
MatthiasChometon <matthiaschometon787@gmail.com>
2023-01-02 17:39:30 +0100

    feat: add CreateGoal and ModifyGoal

--- src/shared/router/Routing.jsx
+++ src/shared/router/Routing.jsx
@@ -9,20 +9,28 @@ import { StudentList } from "../../teacher/studentList/pages/StudentList"
 import { ModuleList } from "../../teacher/moduleList/pages/ModuleList"
 import { CreateModule } from "../../modules/pages/CreateModule"
 import { ModifyModule } from "../../modules/pages/ModifyModule"
+import { CreateGoal } from "../../goals/pages/CreateGoal"
+import { ModifyGoal } from "../../goals/pages/ModifyGoal"
 
 export const Routing = () => {
     return (
         <Routes>
             <Route index path="/account" element={<Account />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/register/:roleName" element={<Register />} />
+
             <Route path="/teacher/student/list" element={<StudentList />} />
             <Route path="/teacher/lessons/list" element={<LessonList />} />
             <Route path="/teacher/modules/list" element={<ModuleList />} />
+
             <Route path="/student/:studentId/goal/:goalId/student-goal/create" element={<CreateStudentGoal />} />
-            <Route path="/student-goal/:goalId" element={<ModifyStudentGoal />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/student-goal/:goalId/modify" element={<ModifyStudentGoal />} />
+
             <Route path="/classroom/:classroomId/modules/create" element={<CreateModule />} />
-            <Route path="/module/:moduleId" element={<ModifyModule />} />
+            <Route path="/module/:moduleId/modify" element={<ModifyModule />} />
+
+            <Route path="/module/:moduleId/goal/create" element={<CreateGoal />} />
+            <Route path="/module/:moduleId/goal/:goalId/modify" element={<ModifyGoal />} />
         </Routes>
     )
 }
