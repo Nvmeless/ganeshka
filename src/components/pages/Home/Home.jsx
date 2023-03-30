6daaf2e41f286db5fbcdd251c008f7059dc46743
julien-guilet <guiletjulien@gmail.com>
2023-03-30 16:55:14 +0200

    nouveau reader

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -52,7 +52,7 @@ export function Home() {
                      }    
               },
               decoder : {
-                  readers : ["code_128_reader","ean_reader", "ean_8_reader","code_39_reader","code_39_vin_reader","codabar_reader","upc_reader","upc_e_reader","i2of5_reader","2of5_reader","code_93_reader"]
+                  readers : ["ean_reader"]
               }
               }, function(err) {
                   if (err) {
